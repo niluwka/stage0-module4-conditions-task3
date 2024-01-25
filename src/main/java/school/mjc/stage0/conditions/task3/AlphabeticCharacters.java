b81f4e4d14a024451cf86148a8d93f977e92e9ba
niluwka0403 <ninimamatova0403@gmail.com>
2024-01-25 20:54:36 +0500

    Multiple Conditions2

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,14 +2,25 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-        boolean test = character.matches("^(?i:[aeiouy]).*");
-        boolean test2 = character.matches("^(?i:[a-zA-Z]).*");
-        if (test) {
+        String s = Character.toString(character);
+        String vowelsRussian = "aeiouy";
+        String consonantEng = "qwrtplkjhgfdszxcvbnm";
+        if (vowelsRussian.indexOf(Character.toLowerCase(s.charAt(0))) != -1) {
             System.out.println("Vowel");
-        }else if (!test2)
-        { System.out.println("wrong alphabet!");}
-        else {
+        } else if (consonantEng.indexOf(Character.toLowerCase(s.charAt(0))) != -1) {
             System.out.println("Consonant");
+        }else {
+            System.out.println("wrong alphabet!");
         }
+
+//        boolean test = character.matches("^(?i:[aeiouy]).*");
+//        boolean test2 = character.matches("^(?i:[a-zA-Z]).*");
+//        if (test) {
+//            System.out.println("Vowel");
+//        }else if (!test2)
+//        { System.out.println("wrong alphabet!");}
+//        else if ( Pattern.matches("[a-zA-Z]", character)){
+//            System.out.println("Consonant");
+//        }
     }
 }
